9677cfcd7b5f4535660d46755d9a952f5dff8fd1
tedy <tedy@r2v.io>
2018-10-17 14:26:10 +0900

    Edit branch id

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ module.exports = Ygg;
 // ygg = new Ygg(new Ygg.providers.HttpProvider("http://localhost:8080"));
 // const timestamp = Math.round(new Date().getTime() / 1000);
 
-/* balance */
+/* get balance */
 
-// let val = ygg.client.getBalance('716af5c8960360c80ab29e2f76638bf299ac0bed', '0xa771a6b5a6cabe2ca35fd55631717d95049d6338');
-// console.log("val", val);
+// let val = ygg.client.getBalance('0a39170899bd7e721730c7c312afc154d784034b', '0xaca4215631187ab5b3af0d4c251fdf45c79ad3c6')
 
+/* get branch */
+
+// ygg.client.getBranchId('YEED');
 
 /* transfer */
 
@@ -23,7 +25,7 @@ module.exports = Ygg;
 // const body = ygg.client.transfer(to, amount);
 // let bodyJson = ygg.utils.dataToJson(body)
 
-// let branchId = "0a39170899bd7e721730c7c312afc154d784034b"
+// let branchId = "3a8171981418e123e2f71c1d155eb2137d9a8264"
 
 // const rawTx = {
 //     "chain":`0x${ygg.utils.hexString(branchId)}`,
@@ -36,14 +38,18 @@ module.exports = Ygg;
 
 // let tx = new ygg.tx(rawTx);
 
-// tx.sign(new Buffer('5654f359c90004451a32dfd0286e61d1944b5a1ecde05808c11138c0c2c26520', 'hex'));
+// tx.sign(new Buffer('3D8A58EA7FA6EF7E038791F3B837FA7BC62DC38CAAFE67AFC4D4567A64D4966E', 'hex'));
 
 // let serialize = tx.serialize(body, branchId);
 
-// ygg.client.sendTransaction(serialize)
+// ygg.client.sendTransaction(serialize).then((result) => {
+//     console.log("hash", result)    
+// })
 
 /* stem */
 
+// let owner = '0xA771A6b5A6cAbE2ca35Fd55631717d95049D6338'
+// let reserve_address = '0xf09e6313f6e5d835bb13d8cbf2e39829083b5d03' //node wallet author
 // var seed = {            
 //          "name": 'YEED',
 //          "symbol": 'YEED',
@@ -53,8 +59,8 @@ module.exports = Ygg;
 //          "tag": 0.1,
 //          "version": '0xcc9612ff91ff844938acdb6608e58506a2f21b8a5d77e88726c0897e8d1d02c0',
 //          "reference_address": '',
-//          "reserve_address": '0xcee3d4755e47055b530deeba062c5bd0c17eb00f',
-//          "owner": '0xA771A6b5A6cAbE2ca35Fd55631717d95049D6338',
+//          "reserve_address": reserve_address,
+//          "owner": owner,
 //          "version_history":['0xcc9612ff91ff844938acdb6608e58506a2f21b8a5d77e88726c0897e8d1d02c0']
 // }
 
